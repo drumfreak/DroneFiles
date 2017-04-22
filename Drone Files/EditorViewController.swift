14cc8d50ea598be06511deaaaca74fc260337426
Eric Rosebrock <webfreakeric@gmail.com>
2017-04-21 21:21:06 -0400

    committing

---
+++ Drone Files/EditorViewController.swift
@@ -0,0 +1,50 @@
+//
+//  EditorViewController.swift
+//  Drone Files
+//
+//  Created by Eric Rosebrock on 4/20/17.
+//  Copyright © 2017 The Web Freaks, INC. All rights reserved.
+//
+
+import Foundation
+
+import Cocoa
+import AVKit
+import AppKit
+import AVFoundation
+
+
+class EditorViewController: NSViewController {
+    
+    @IBOutlet var mainContainerView: NSView!
+    
+    @IBOutlet weak var screenShotViewController: ScreenshotViewController!
+    @IBOutlet weak var videoPlayerViewController: VideoPlayerViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // print("Subview Loded")
+        print("Editor Container Loaded")
+        
+        self.performSegue(withIdentifier: "videoPlayerSegue", sender: self)
+    }
+
+    override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+        print("SEGUE");
+        print(segue.identifier!)
+        if segue.identifier == "videoPlayerSegue" {
+            self.videoPlayerViewController = segue.destinationController as! VideoPlayerViewController
+            print ("Videos Loaded by segue");
+        }
+        
+        if segue.identifier == "screenShotSave" {
+            
+            print ("Screen Shots Loaded by segue");
+        }
+    }
+
+}
+
+
+
+
