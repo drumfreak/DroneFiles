f67a2e8ede61e553601e22a43f04fad4b792b847
Eric Rosebrock <webfreakeric@gmail.com>
2017-04-21 21:21:20 -0400

    Committing

---
+++ Drone Files/MainContainerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainContainerViewController.swift
+//  Drone Files
+//
+//  Created by Eric Rosebrock on 4/20/17.
+//  Copyright © 2017 The Web Freaks, INC. All rights reserved.
+//
+
+import Foundation
+
+import Cocoa
+import AVKit
+import AppKit
+import AVFoundation
+
+
+class MainContainerViewController: NSViewController {
+    
+    @IBOutlet var mainContainerView: NSView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // print("Subview Loded")
+    }
+}
